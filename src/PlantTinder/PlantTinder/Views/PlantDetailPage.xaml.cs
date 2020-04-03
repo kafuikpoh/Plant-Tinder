0df37a54115689a3ecf30905b92282c542ec74ef
Kafui Kpoh <qaphuikpoh@gmail.com>
2020-04-03 21:33:37 +0000

    Add PlantDetailPage

---
+++ src/PlantTinder/PlantTinder/Views/PlantDetailPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using PlantTinder.Models;
+using PlantTinder.ViewModels;
+using Xamarin.Forms;
+
+namespace PlantTinder.Views
+{
+    public partial class PlantDetailPage : ContentPage
+    {
+        public PlantDetailPage(Plant plant)
+        {
+            InitializeComponent();
+
+            BindingContext = new PlantDetailViewModel(plant);
+        }
+
+        async void BackIcon_Tapped(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
